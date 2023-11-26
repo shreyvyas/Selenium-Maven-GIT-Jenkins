f8a5eda89dec07122c2f40b62e2beb76ae2569f1
shreyvyas <shreyvyas@gmail.com>
2023-11-26 13:43:47 +0530

    commit

---
+++ src/code/BrowserInvoke.java
@@ -0,0 +1,28 @@
+package code;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserInvoke {
+
+	public static void main(String[] args) {
+		
+		//this step is not required from 4.6, selenium manager can internally invoke the browser
+		//chrome
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium-Setup\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		//firefox
+//		System.setProperty("webdriver.chrome.driver", "C:\\Selenium-Setup\\drivers\\geckodriver.exe");
+//		
+//		WebDriver driver1 = new FirefoxDriver();
+		
+		driver.get("https://www.google.com/");
+		
+		driver.quit();
+
+	}
+
+}
